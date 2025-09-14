bc172be14e4e7aae923dcc718cccc79b0535c849
DY <malagebidi@gmail.com>
2025-09-14 18:18:08 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
     './content/**/*.md',
     './themes/**/layouts/**/*.html'
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), // <-- 启用插件
+  ],
 }
